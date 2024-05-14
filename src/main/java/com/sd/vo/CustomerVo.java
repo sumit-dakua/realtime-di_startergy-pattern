b9f24051513d23085e0f3078c8e130848668b59b
sumit-dakua <sumitdakua4@gmail.com>
2024-05-14 20:45:02 +0530

    add value object file(VO)

---
+++ src/main/java/com/sd/vo/CustomerVo.java
@@ -0,0 +1,61 @@
+package com.sd.vo;
+
+public class CustomerVo {
+
+  private String customerNo;
+  private String customerName;
+  private String customerAddress;
+  private String principleAmount;
+  private String rateOfInterest;
+  private String timePeriod;
+
+  // setters and getters
+  public String getCustomerNo() {
+    return customerNo;
+  }
+
+  public void setCustomerNo(String customerNo) {
+    this.customerNo = customerNo;
+  }
+
+  public String getCustomerName() {
+    return customerName;
+  }
+
+  public void setCustomerName(String customerName) {
+    this.customerName = customerName;
+  }
+
+  public String getCustomerAddress() {
+    return customerAddress;
+  }
+
+  public void setCustomerAddress(String customerAddress) {
+    this.customerAddress = customerAddress;
+  }
+
+  public String getPrincipleAmount() {
+    return principleAmount;
+  }
+
+  public void setPrincipleAmount(String principleAmount) {
+    this.principleAmount = principleAmount;
+  }
+
+  public String getRateOfInterest() {
+    return rateOfInterest;
+  }
+
+  public void setRateOfInterest(String rateOfInterest) {
+    this.rateOfInterest = rateOfInterest;
+  }
+
+  public String getTimePeriod() {
+    return timePeriod;
+  }
+
+  public void setTimePeriod(String timePeriod) {
+    this.timePeriod = timePeriod;
+  }
+
+}
