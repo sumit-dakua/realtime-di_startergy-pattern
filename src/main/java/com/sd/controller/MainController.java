11fc3dc6afa86f81b13c423932484a44f81af37d
sumit-dakua <sumitdakua4@gmail.com>
2024-05-15 14:50:35 +0530

    update MainController file

--- src/main/java/com/sd/controller/MainController.java
+++ src/main/java/com/sd/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.sd.controller;
 
+import com.sd.dto.CustomerDto;
 import com.sd.service.IcustomerService;
+import com.sd.vo.CustomerVo;
 
 /**
  * MainController
@@ -11,6 +13,20 @@ public class MainController {
 
   public MainController(IcustomerService service) {
     this.service = service;
+  }
+
+  public String processCutomer(CustomerVo custVo) throws Exception {
+
+    CustomerDto dto = new CustomerDto();
+    dto.setCustomerName(custVo.getCustomerName());
+    dto.setCustomerAddress(custVo.getCustomerAddress());
+    dto.setPrincipleAmount(custVo.getPrincipleAmount());
+    dto.setRateOfInterest(custVo.getRateOfInterest());
+    dto.setTimePeriod(custVo.getTimePeriod());
+
+    // using service
+    String resultMsg = service.registerCustomer(dto);
+    return resultMsg;
   }
 
 }
