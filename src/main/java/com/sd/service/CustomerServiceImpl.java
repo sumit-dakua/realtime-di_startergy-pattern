8b1fe044c160ba3322529c3d4544243ba4cdab7d
sumit-dakua <sumitdakua4@gmail.com>
2024-05-15 16:27:06 +0530

    add full service class

--- src/main/java/com/sd/service/CustomerServiceImpl.java
+++ src/main/java/com/sd/service/CustomerServiceImpl.java
@@ -1,12 +1,31 @@
 package com.sd.service;
 
+import com.sd.bo.CustomerBo;
+import com.sd.dao.ICustomerDao;
 import com.sd.dto.CustomerDto;
 
 public class CustomerServiceImpl implements IcustomerService {
 
+  private ICustomerDao dao;
+
+  public CustomerServiceImpl(ICustomerDao dao) {
+    this.dao = dao;
+  }
+
   public String registerCustomer(CustomerDto custDto) throws Exception {
 
-    return "msg";
+    double interestAmount = (custDto.getPrincipleAmount() * custDto.getRateOfInterest() * custDto.getTimePeriod())
+        / 100;
+
+    CustomerBo bo = new CustomerBo();
+    bo.setCustomerName(custDto.getCustomerName());
+    bo.setCustomerAddress(custDto.getCustomerAddress());
+    bo.setPrincipleAmount(custDto.getPrincipleAmount());
+    bo.setInterestAmount(interestAmount);
+
+    int count = dao.insert(bo);
+
+    return count == 0 ? "Registration failed " : "Registration succeed : : interest amount ->" + interestAmount;
 
   }
 }
